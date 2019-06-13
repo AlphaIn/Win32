5886c9496260dc63ddbf9a8e89abdb364e3f86e3
AlphaIn <mymail_123@yeah.net>
2019-06-13 16:50:32 +0800

    Initial commit

---
+++ WinDlg/WinDlg/WinDlg.cpp
@@ -0,0 +1,262 @@
+﻿// WinDlg.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "WinDlg.h"
+#include "resource.h"
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WINDLG, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDLG));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDLG));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDLG);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+int CALLBACK DialogProc(HWND hwndlg, UINT msgID, WPARAM wParam, LPARAM IParam)
+{
+	switch (msgID)
+	{
+	case WM_SYSCOMMAND:
+		if (wParam == SC_CLOSE)
+		{
+			EndDialog(hwndlg, 1001);//销毁模式对话框
+		}
+		break;
+	}
+	return FALSE;//将消息交给操作系统处理
+}
+int CALLBACK DlgProc(HWND hwndlg, UINT msgID, WPARAM wParam, LPARAM IParam) 
+{
+	switch (msgID)
+	{
+	case WM_SYSCOMMAND:
+		if (wParam == SC_CLOSE)
+		{
+			DestroyWindow(hwndlg);//销毁模式对话框
+		}
+		break;
+	}
+	return FALSE;
+}
+void OnPaint(HWND hWnd)
+{
+	//画布初始化
+	PAINTSTRUCT ps = { 0 };
+	HDC hdc = BeginPaint( hWnd, &ps );
+	//画笔
+	HPEN nPen = CreatePen(PS_SOLID,2,RGB(255,0,0));
+	//设置新画笔，返回值为默认黑色画笔
+	HGDIOBJ nOldPen = SelectObject(hdc, nPen);
+	//实心画刷
+	//HBRUSH nBrush = CreateSolidBrush(RGB(0,0,255));
+	//阴影画刷
+	HBRUSH nBrush = CreateHatchBrush(HS_CROSS, RGB(0, 0, 255));
+	//设置新画刷，返回值为默认白色画刷
+	HGDIOBJ nOldBrush = SelectObject(hdc,nBrush);
+	//画矩形
+	Rectangle(hdc, 100, 100, 300, 300);
+	//归还（设置）画笔为原画笔
+	SelectObject(hdc, nOldPen);
+	DeleteObject(nPen);
+	SelectObject(hdc, nOldBrush);
+	DeleteObject(nBrush);
+	EndPaint(hWnd, &ps);
+
+}
+void OnModel(HWND hWnd)
+{
+	int nRet = DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, DialogProc);
+	if (nRet == 1001)
+	{
+		MessageBox(hWnd, _T("succsess"), _T("Info"), MB_OK);
+	}
+}
+void OnNoModel(HWND hWnd) {
+	HWND hDlg = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, DlgProc);
+	ShowWindow(hDlg, SW_SHOW);
+}
+void OnCommand(HWND hWnd, WPARAM wParam) 
+{
+	switch (LOWORD(wParam))
+	{
+	case ID_MODEL:
+		OnModel(hWnd);//创建模式对话框
+		break;
+	case ID_NO_MODEL:
+		OnNoModel(hWnd);
+		break;
+	case IDM_ABOUT:
+		DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+		break;
+	case IDM_EXIT:
+		DestroyWindow(hWnd);
+		break;
+	}
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+		OnCommand(hWnd, wParam);
+        /*{
+            int wmId = LOWORD(wParam);
+            // 分析菜单选择:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }*/
+        break;
+    case WM_PAINT:
+		OnPaint(hWnd);
+		break;
+            /*PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 在此处添加使用 hdc 的任何绘图代码...
+            EndPaint(hWnd, &ps);*/
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
